a2bc63ea76f914c20ced5599871a963b95a953e8
Pawel Lisewski <dev@palekiwi.com>
2024-11-28 21:04:46 +0800

    Search templates for controller method

--- lua/rails-utils/lib.lua
+++ lua/rails-utils/lib.lua
@@ -4,32 +4,48 @@ M = {}
 
 --- tries to locate files which render the template in current buffer
 M.find_template_render = function()
-  if vim.bo.filetype ~= "eruby" then return end
+  if vim.bo.filetype == "eruby" then
+    local dir, filename = string.match(vim.fn.expand("%:r:r"), "app/views/([^ ]*)/_?([^%s]*)")
 
-  local dir, filename = string.match(vim.fn.expand("%:r:r"), "app/views/([^ ]*)/_?([^%s]*)")
+    local controller_file = "app/controllers/" .. dir .. "_controller.rb"
+    local cmd = "rg -n 'def " .. filename .. "' " .. controller_file
 
-  local controller_file = "app/controllers/" .. dir .. "_controller.rb"
-  local cmd = "rg -n 'def " .. filename .. "' " .. controller_file
+    local handle = assert(io.popen(cmd))
+    local result = handle:read("*a")
+    handle:close()
 
-  local handle = assert(io.popen(cmd))
-  local result = handle:read("*a")
-  handle:close()
+    local line = result:match("^%d+")
 
-  local line = result:match("^%d+")
+    if line ~= nil then
+      return vim.cmd(string.format("e +%s %s", line, controller_file))
+    end
 
-  if line ~= nil then
-    return vim.cmd(string.format("e +%s %s", line, controller_file))
+    local regex = "((\\s+(render|partial:)\\s*\\(?)|^\\s*)[\'\"](" .. dir .. "/)?" .. filename .. "[\'\"]"
+
+    builtin.grep_string(
+      {
+        search_dirs = { "app/controllers", "app/views" },
+        search = regex,
+        use_regex = true
+      }
+    )
+  elseif vim.bo.filetype == "ruby" then
+    local dir, filename = string.match(vim.fn.expand("%:r:r"), "app/controllers/?([^ ]*)/([^%s]*)_controller")
+    local method = vim.fn.expand("<cword>")
+    local path
+
+    if dir ~= "" then
+      path = "app/views/" .. dir  .. "/" .. filename .. "/" .. method .. ".html.erb"
+    else
+      path = "app/views/" .. filename .. "/" .. method .. ".html.erb"
+    end
+
+    if vim.fn.filereadable(path) == 1 then
+      vim.cmd("e " .. path)
+    else
+      print("File " .. path .. " does not exist.")
+    end
   end
-
-  local regex = "((\\s+(render|partial:)\\s*\\(?)|^\\s*)[\'\"](" .. dir .. "/)?" .. filename .. "[\'\"]"
-
-  builtin.grep_string(
-    {
-      search_dirs = { "app/controllers", "app/views" },
-      search = regex,
-      use_regex = true
-    }
-  )
 end
 
 --- list templates with file names matching the word under cursor
